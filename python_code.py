a66673f353d2c8050cb8eee2da6cd1116bf8ebc5
Analyst Geek <44949175+AnalystGeek@users.noreply.github.com>
2023-09-22 10:18:06 -0400

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -1,1 +1,2 @@
-import streamlit as stst.write("Hello ,let's learn how to build a streamlit app together")
+import streamlit as st
+st.write("Hello ,let's learn how to build a streamlit app together")
