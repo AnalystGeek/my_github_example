20644bca4fdb7efa32f4903844f320c8f20a3328
Analyst Geek <44949175+AnalystGeek@users.noreply.github.com>
2023-09-22 10:25:41 -0400

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 st.write("Streatlit example of dataframe")
 
-df_deduct_fact = pd.read_excel (r"C:\Users\AZA97096\OneDrive - Mott MacDonald\Documents\Project Docs\Mississippi DOT-PMS\Distress Data\deduct_factors.xlsx")
+df = pd.DataFrame(
+   np.random.randn(50, 20),
+   columns=('col %d' % i for i in range(20)))
 
-st.dataframe(df_deduct_fact)
+st.dataframe(df)
