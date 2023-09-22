143a85336d48e76b1eb5a05bf120b77b07a1e347
Analyst Geek <44949175+AnalystGeek@users.noreply.github.com>
2023-09-22 16:27:55 -0400

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -2,15 +2,21 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-##st.write("Streatlit example of dataframe")
-##df = pd.read_excel (".master/deduct_factors.xlsx")
-##df = pd.DataFrame(
-  ## np.random.randn(50, 20),
-   ## columns=('col %d' % i for i in range(20)))
+# Cache the dataframe so it's only loaded once
+@st.cache_data
+def load_data():
+    return pd.DataFrame(
+        {
+            "first column": [1, 2, 3, 4],
+            "second column": [10, 20, 30, 40],
+        }
+    )
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file)
-  st.write(df)
-  
-##st.dataframe(df)
+# Boolean to resize the dataframe, stored as a session state variable
+st.checkbox("Use container width", value=False, key="use_container_width")
+
+df = load_data()
+
+# Display the dataframe and allow the user to stretch the dataframe
+# across the full width of the container, based on the checkbox value
+st.dataframe(df, use_container_width=st.session_state.use_container_width
