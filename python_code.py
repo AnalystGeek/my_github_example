ec68a8c9a2936103477262b14c44c69b022d9116
Analyst Geek <44949175+AnalystGeek@users.noreply.github.com>
2023-09-22 10:52:05 -0400

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -3,9 +3,14 @@ import pandas as pd
 import numpy as np
 
 st.write("Streatlit example of dataframe")
-df = pd.read_excel (".master/deduct_factors.xlsx")
+##df = pd.read_excel (".master/deduct_factors.xlsx")
 ##df = pd.DataFrame(
   ## np.random.randn(50, 20),
    ## columns=('col %d' % i for i in range(20)))
 
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+  df = pd.read_excel(uploaded_file)
+  ##st.write(df)
+  
 st.dataframe(df)
