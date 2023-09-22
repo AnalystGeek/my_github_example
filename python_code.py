78d6c27852b237b8561d4809ff72f3da80ec7d03
Analyst Geek <44949175+AnalystGeek@users.noreply.github.com>
2023-09-22 10:22:52 -0400

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -1,2 +1,8 @@
 import streamlit as st
-st.write("Hello ,let's learn how to build a streamlit app together")
+import pandas as pd
+
+st.write("Streatlit example of dataframe")
+
+df_deduct_fact = pd.read_excel (r"C:\Users\AZA97096\OneDrive - Mott MacDonald\Documents\Project Docs\Mississippi DOT-PMS\Distress Data\deduct_factors.xlsx")
+
+st.dataframe(df_deduct_fact)
