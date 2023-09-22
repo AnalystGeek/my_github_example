f3617fc2e0547e156aaa25f1fc0b4e6f9e3b1e63
Analyst Geek <44949175+AnalystGeek@users.noreply.github.com>
2023-09-22 10:53:54 -0400

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -10,7 +10,7 @@ st.write("Streatlit example of dataframe")
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-  df = pd.read_excel(uploaded_file)
+  df = pd.read_csv(uploaded_file)
   st.write(df)
   
 ##st.dataframe(df)
