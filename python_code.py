402794e1fef37ce3fbe3501e0846668f153180e8
Analyst Geek <44949175+AnalystGeek@users.noreply.github.com>
2023-09-22 10:34:48 -0400

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -3,9 +3,9 @@ import pandas as pd
 import numpy as np
 
 st.write("Streatlit example of dataframe")
-
-df = pd.DataFrame(
-   np.random.randn(50, 20),
-   columns=('col %d' % i for i in range(20)))
+df = pd.read_excel ("./deduct_factors.xlsx")
+##df = pd.DataFrame(
+  ## np.random.randn(50, 20),
+   ## columns=('col %d' % i for i in range(20)))
 
 st.dataframe(df)
